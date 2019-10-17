53ab8454b05049b89218e1b3852e16b50f5265cb
Group 2 <ja2416@ic.ac.uk>
2019-10-17 10:55:50 +0100

    [ja2416]: Initial Commit

---
+++ square_path.py
@@ -0,0 +1,46 @@
+import brickpi3
+BP = brickpi3.BrickPi3()
+
+# Reset motor encoders to 0
+def reset_encoders():
+    BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
+    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+
+
+# Get current encoding
+def get_encoding():
+    encoder_a = BP.get_motor_encoder(BP.PORT_A)
+    encoder_b = BP.get_motor_encoder(BP.PORT_B)
+
+    return (encoder_a, encoder_b)
+
+# Drive in straight line
+def drive_straight(cm):
+    encoder_a, encoder_b = get_encoding()
+
+    BP.set_motor_power(BP.PORT_A, -50)
+    BP.set_motor_power(BP.PORT_B, -50)
+    while(encoder_a > -1000 and encoder_b > -1000):
+        encoder_a, encoder_b = get_encoding()
+
+    BP.set_motor_power(BP.PORT_A, 0)
+    BP.set_motor_power(BP.PORT_B, 0)
+
+def turn_right():
+    encoder_a, encoder_b = get_encoding()
+
+    BP.set_motor_power(BP.PORT_A, -50)
+    BP.set_motor_power(BP.PORT_B, 50)
+    while(encoder_a > -700):
+        encoder_a, encoder_b = get_encoding()
+
+    BP.set_motor_power(BP.PORT_A, 0)
+    BP.set_motor_power(BP.PORT_B, 0)
+
+
+if(__name__ == "__main__"):
+    reset_encoders()
+    drive_straight(5)
+    reset_encoders()
+    turn_right()
+
